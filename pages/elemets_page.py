ce4d263ae27363405d6fb90a37f56294e36c485a
WeDev <webdevjs2022@gmail.com>
2024-07-03 10:15:56 +0500

    Генератор данных

--- pages/elemets_page.py
+++ pages/elemets_page.py
@@ -1,6 +1,7 @@
 from selenium.webdriver import Keys
 
 from conftest import driver
+from generator.generator import generated_person
 from locators.elements_page_locators import TextBoxPageLocators
 from pages.base_page import BasePage
 
@@ -9,11 +10,17 @@ class TextBoxPage(BasePage):
     locators = TextBoxPageLocators()
 
     def fill_all_fields(self, driver):
-        self.element_is_visible(self.locators.FULL_NAME).send_keys('Anna')
-        self.element_is_visible(self.locators.EMAIL).send_keys('anna@gmail.com')
-        self.element_is_visible(self.locators.CURRENT_ADDRESS).send_keys('London-Paris')
-        self.element_is_visible(self.locators.PERMANENT_ADDRESS).send_keys('Paris-London')
+        person_info = next(generated_person())
+        full_name = person_info.full_name
+        email = person_info.email
+        current_address = person_info.current_address
+        permanent_address = person_info.permanent_address
+        self.element_is_visible(self.locators.FULL_NAME).send_keys(full_name)
+        self.element_is_visible(self.locators.EMAIL).send_keys(email)
+        self.element_is_visible(self.locators.CURRENT_ADDRESS).send_keys(current_address)
+        self.element_is_visible(self.locators.PERMANENT_ADDRESS).send_keys(permanent_address)
         self.element_is_visible(self.locators.SUBMIT).send_keys(Keys.ENTER)
+        return full_name, email, current_address, permanent_address
 
     def check_filled_form(self):
         full_name = self.element_is_present(self.locators.CREATED_FULL_NAME).text.split(':')[1]
--- tests/elements_test.py
+++ tests/elements_test.py
@@ -11,10 +11,11 @@ class TestElements:
             text_box_page = TextBoxPage(driver, 'https://demoqa.com/text-box')
             text_box_page.open()
             driver.execute_script("window.scrollTo(0, 700)")  # СКРОЛЛИНГ
-            text_box_page.fill_all_fields(self)
+            full_name, email, current_address, permanent_address = text_box_page.fill_all_fields(self)
             time.sleep(1)
             output_name, output_email, output_cur_addr, output_per_addr = text_box_page.check_filled_form()
-            print(output_name)
-            print(output_email)
-            print(output_cur_addr)
-            print(output_per_addr)
+            time.sleep(2)
+            assert full_name == output_name, "the full name does not match"
+            assert email == output_email, "the email does not match"
+            assert current_address == output_cur_addr, "the current address does not match"
+            assert permanent_address == output_per_addr, "the permanent address does not match"
