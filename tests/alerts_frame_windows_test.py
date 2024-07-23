859d931705d168024323983c0f45b44535004aba
WeDev <webdevjs2022@gmail.com>
2024-07-23 09:59:00 +0500

    added test FramesPage

--- tests/alerts_frame_windows_test.py
+++ tests/alerts_frame_windows_test.py
@@ -1,7 +1,6 @@
-import time
 from conftest import driver
 
-from pages.alerts_frame_windows_page import BrowserWindowsPage, AlertsPage
+from pages.alerts_frame_windows_page import BrowserWindowsPage, AlertsPage, FramesPage
 
 
 class TestAlertsFrameWindow:
@@ -46,3 +45,13 @@ class TestAlertsFrameWindow:
             text, alert_text = alert_page.check_prompt_alert()
             assert alert_text == f'You entered {text}', "Alert not prompted"
             # or assert text in alert_text
+
+    class TestFramesPage:
+
+        def test_frames(self, driver):
+            frame_page = FramesPage(driver, 'https://demoqa.com/frames')
+            frame_page.open()
+            result_frame1 = frame_page.check_frame('frame1')
+            result_frame2 = frame_page.check_frame('frame2')
+            assert result_frame1 == ['This is a sample page', '500px', '350px'], "The frame does not exist"
+            assert result_frame2 == ['This is a sample page', '100px', '100px'], "The frame does not exist"
