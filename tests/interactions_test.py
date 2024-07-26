ca029c92921abbef75d1c4f7882612b651c33e95
WeDev <webdevjs2022@gmail.com>
2024-07-26 09:31:26 +0500

    added test SelectablePage

--- tests/interactions_test.py
+++ tests/interactions_test.py
@@ -1,5 +1,5 @@
 from conftest import driver
-from pages.interactions_page import SortablePage
+from pages.interactions_page import SortablePage, SelectablePage
 
 
 class TestInteractions:
@@ -13,3 +13,12 @@ class TestInteractions:
             grid_before, grid_after = sortable_page.change_grid_order()
             assert list_before != list_after, "the order of the list has not been changed"
             assert grid_before != grid_after, "the order of the grid has not been changed"
+
+    class TestSelectablePage:
+        def test_selectable(self, driver):
+            selectable_page = SelectablePage(driver, 'https://demoqa.com/selectable')
+            selectable_page.open()
+            item_list = selectable_page.select_list_item()
+            item_grid = selectable_page.select_grid_item()
+            assert len(item_list) > 0, "no elements are selected"
+            assert len(item_grid) > 0, "no elements are selected"
